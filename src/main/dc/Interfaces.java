d34c5e03d25d1cb4bfe03df3658c98968cef1aeb
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-10-02 22:42:02 +0100

    added interfaces to respective directory

---
+++ src/main/dc/Interfaces.java
@@ -0,0 +1,18 @@
+/* Copyright (C) Harry Clark */
+
+/* SEGA Dreamcast GDI Tool for GHIDRA */
+
+/* THIS FILE JUST PROVIDES THE NUANCES FOR THE RETURN FUNCTIONS */
+/* ASSOCIATED WITH ALLOCATING THE NAME AND ADDRESS TO THEIR RESPECTIVE */
+/* FUNCTION CALLS */
+
+package main.dc;
+
+public interface Interfaces
+{
+    public interface I_ADDRESS{};
+    public interface I_NAME{};
+
+    public static class VECTOR_FUNC{};
+
+}
